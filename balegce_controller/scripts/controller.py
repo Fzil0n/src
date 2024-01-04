4889b6fee279917c205e4ea6e0db64347f40f8f4
Fzil0n <tanawat84598@gmail.com>
2024-01-04 18:03:03 +0700

    add controller v1

--- balegce_controller/scripts/controller.py
+++ balegce_controller/scripts/controller.py
@@ -21,10 +21,15 @@ class controller(Node):
         self.create_subscription(Twist, 'euler_angles', self.orientation_callback, 10)
 
         # Variables
-        self.orientation = [0.0, 0.0, 0.0]
+        self.orientation    = [0.0, 0.0, 0.0]
+        self.referenceAngles = [0.0, 0.0, 0.0]
+        self.Kp_wheel       = 1.0
+        self.Kp_propellerL  = 1.0
+        self.Kp_propellerR  = 1.0
+        self.forceConstance = 1.0
 
     # Methods ===========================================
-    def wrenchPub(self, publisher, force, torque):
+    def wrenchPub(self, publisher, force:list[float], torque:list[float])->None:
         msg = Wrench()
         # force assignment
         msg.force.x = force[0]
@@ -39,29 +44,42 @@ class controller(Node):
     
     # Timer Callback -----------------------------
     def timerCallback(self):
+        controller_output = self.velocityController()
         # position
         pubPos = Float64MultiArray()
         pubPos.data = [0.0]
         # velocity
         pubVelo = Float64MultiArray()
-        pubVelo.data = [0.0, 0.0]
+        # wheel prop1(left) prop2(right) leg
+        pubVelo.data = [controller_output[0], controller_output[1], controller_output[2], 0.0]
         # publish
+        self.wrenchPub(self.pub_forceL, force=[0.0, 0.0, -controller_output[3]], torque=[0.0, 0.0, 0.0])
+        self.wrenchPub(self.pub_forceR, force=[0.0, 0.0, -controller_output[4]], torque=[0.0, 0.0, 0.0])
         self.pub_posCommand.publish(pubPos)
         self.pub_veloCommand.publish(pubVelo)
 
     # Subscriber Callback ------------------------
     def orientation_callback(self, msg):
-        self.orientation[0] = msg.angular.roll_x
-        self.orientation[1] = msg.angular.pitch_y
-        self.orientation[2] = msg.angular.yaw_z
+        self.orientation[0] = msg.angular.x
+        self.orientation[1] = msg.angular.y
+        self.orientation[2] = msg.angular.z
 
     # Controller ---------------------------------
-    def controller(self):
+    def velocityController(self)->list[float]:
         # orientation error
-        diff_orient_x = 0 - self.orientation[0]
-        diff_orient_y = 0 - self.orientation[1]
-        diff_orient_z = 0 - self.orientation[2]
-        
+        diff_orient_x = self.referenceAngles[0] - self.orientation[0]
+        diff_orient_y = self.referenceAngles[1] - self.orientation[1]
+        diff_orient_z = self.referenceAngles[2] - self.orientation[2]
+        # controller
+        wheel_velo = self.Kp_wheel*diff_orient_x 
+        propellerR_velo = self.Kp_propellerR*diff_orient_y + self.Kp_propellerR*diff_orient_z
+        propellerL_velo = self.Kp_propellerL*diff_orient_y - self.Kp_propellerL*diff_orient_z
+        # velocityToforce
+        propellerR_force = self.forceConstance*propellerR_velo
+        propellerL_force = self.forceConstance*propellerL_velo
+        output = [wheel_velo, propellerL_velo, propellerR_velo, propellerL_force, propellerR_force]
+        return output
+
 
 def main(args=None):
     rclpy.init(args=args)
