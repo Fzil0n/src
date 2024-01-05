475d0819943240826c4f339753f925e4c260f33a
Fzil0n <tanawat84598@gmail.com>
2024-01-05 15:51:05 +0700

    add launch argus for controller gain

--- balegce_controller/scripts/controller.py
+++ balegce_controller/scripts/controller.py
@@ -19,14 +19,15 @@ class controller(Node):
 
         # Create Subscriber
         self.create_subscription(Twist, 'euler_angles', self.orientation_callback, 10)
-
+        # ROS Parameters
+        self.declare_parameter('Kp_wheel',1.0)
+        self.declare_parameter('Kp_propellerL',1.0)
+        self.declare_parameter('Kp_propellerR',1.0)
+        self.declare_parameter('forceConstance',1.0)
         # Variables
         self.orientation    = [0.0, 0.0, 0.0]
         self.referenceAngles = [0.0, 0.0, 0.0]
-        self.Kp_wheel       = 1.0
-        self.Kp_propellerL  = 1.0
-        self.Kp_propellerR  = 1.0
-        self.forceConstance = 1.0
+
 
     # Methods ===========================================
     def wrenchPub(self, publisher, force:list[float], torque:list[float])->None:
@@ -66,17 +67,21 @@ class controller(Node):
 
     # Controller ---------------------------------
     def velocityController(self)->list[float]:
+        Kp_wheel       = self.get_parameter('Kp_wheel').value
+        Kp_propellerL  = self.get_parameter('Kp_propellerL').value
+        Kp_propellerR  = self.get_parameter('Kp_propellerR').value
+        forceConstance = self.get_parameter('forceConstance').value
         # orientation error
         diff_orient_x = self.referenceAngles[0] - self.orientation[0]
         diff_orient_y = self.referenceAngles[1] - self.orientation[1]
         diff_orient_z = self.referenceAngles[2] - self.orientation[2]
         # controller
-        wheel_velo = self.Kp_wheel*diff_orient_x 
-        propellerR_velo = self.Kp_propellerR*diff_orient_y + self.Kp_propellerR*diff_orient_z
-        propellerL_velo = self.Kp_propellerL*diff_orient_y - self.Kp_propellerL*diff_orient_z
+        wheel_velo = Kp_wheel*diff_orient_x 
+        propellerR_velo = Kp_propellerR*diff_orient_y + Kp_propellerR*diff_orient_z
+        propellerL_velo = Kp_propellerL*diff_orient_y - Kp_propellerL*diff_orient_z
         # velocityToforce
-        propellerR_force = self.forceConstance*propellerR_velo
-        propellerL_force = self.forceConstance*propellerL_velo
+        propellerR_force = forceConstance*propellerR_velo
+        propellerL_force = forceConstance*propellerL_velo
         output = [wheel_velo, propellerL_velo, propellerR_velo, propellerL_force, propellerR_force]
         return output
 
